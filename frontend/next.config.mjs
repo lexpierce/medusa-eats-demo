8d13a729558e95519df87eda7c229e98a0a489cb
Alexander Sisco <alexander@render.com>
2024-09-17 12:48:28 -0700

    update URL for images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,12 +2,24 @@
 const nextConfig = {
   images: {
     // Disable the default Image Optimization API for this demo.
-    unoptimized: true,
+    // unoptimized: true,
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-eats-frontend.onrender.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-eats-frontend-blueprint.onrender.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-eats-demo-blueprint.onrender.com",
       },
       {
         protocol: "https",
